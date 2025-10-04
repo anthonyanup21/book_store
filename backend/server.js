2cc9c88804441dd6fa0ea1f67d564f4c717083aa
anthony anup <anup>
2025-10-04 23:47:00 +0530

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,20 +6,26 @@ import cookieParser from "cookie-parser"
 import bookRoute from "./routes/book.route.js"
 import reviewRoute from "./routes/review.route.js"
 import cors from "cors"
+import path from "path"
+
 dotenv.config()
 
 
 const app = express()
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+if(process.env.ENV!="production"){
+    app.use(cors({origin:"http://localhost:5173",credentials:true}))
+
+}
 app.use(cookieParser())
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/book", bookRoute)
 app.use("/api/review", reviewRoute)
 
+if(process.env.ENV=="production"){
+app.get(/.*/,(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+})}
 
 
 connectDB().then(() => {
--- frontend/src/axios/axiosInstance.js
+++ frontend/src/axios/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL="http://localhost:3000/api"
+const BASE_URL=process.env.ENV=="production"?"/api":"http://localhost:3000/api"
 const axiosInstance=axios.create({
     baseURL:BASE_URL,
     withCredentials:true
