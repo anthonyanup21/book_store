84c3e04c456d04ea68b362a8419de7291c314394
anthony anup <anup>
2025-10-05 07:59:34 +0530

    removed build folder

--- frontend/src/axios/axiosInstance.js
+++ frontend/src/axios/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios"
-const BASE_URL=import.meta.env.ENV!="development"?"/api":"http://localhost:3000/api"
+const BASE_URL=import.meta.env?.ENV!="development"?"/api":"http://localhost:3000/api"
 const axiosInstance=axios.create({
     baseURL:BASE_URL,
     withCredentials:true
 })
-
+console.log(BASE_URL)
 export default axiosInstance
